f95c9c81b8b2fd8d39af8386c383a18e16269761
MdHelalUddinBiswas <helal@nexstack.sg>
2025-05-16 15:59:06 +0600

    Create Express adapter for Vercel serverless environment

--- api/index.js
+++ api/index.js
@@ -1,6 +1,12 @@
-// The absolute minimum serverless function
+// Import the main Express app
+const app = require('../index');
 
+// This is a special handler for Vercel serverless functions
+// It acts as an adapter between Vercel's serverless environment and Express
 module.exports = (req, res) => {
-  // Just return a static text response
-  res.status(200).send('MeetNing API is running');
+  // Mark request as coming from Vercel
+  req.isVercel = true;
+  
+  // Forward the request to the Express app
+  return app(req, res);
 };
